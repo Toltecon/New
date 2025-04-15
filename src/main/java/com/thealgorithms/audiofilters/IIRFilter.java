0b34fddc6532e3c5f9a7dbcd1a916615275f9a44
Toltecon <147872448+Toltecon@users.noreply.github.com>
2025-04-15 11:43:04 +0300

    Update IIRFilter.java
    485

--- src/main/java/com/thealgorithms/audiofilters/IIRFilter.java
+++ src/main/java/com/thealgorithms/audiofilters/IIRFilter.java
@@ -1,7 +1,7 @@
 package com.thealgorithms.audiofilters;
 
 /**  
- * N-Order IIR Filter Assumes inputs are normalized to [-1, 1]
+ * N-Order IIR Filter Assumes inputs are normalized to [-1, 1] 
  *
  * Based on the difference equation from 
  * <a href="https://en.wikipedia.org/wiki/Infinite_impulse_response">Wikipedia link</a>
